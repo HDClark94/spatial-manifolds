60fdda1ec9de5a8230c4dc50eaa17a3757679a35
HDClark94 <harrydclark91@gmail.com>
2025-07-01 17:54:08 +0100

    update to wolfs most recent curation measures

--- src/spatial_manifolds/data/curation.py
+++ src/spatial_manifolds/data/curation.py
@@ -1,21 +1,23 @@
 import warnings
+import pynapple as nap
 
-
-def curate_clusters(clusters):
+def curate_clusters(clusters) -> nap.TsGroup:
     with warnings.catch_warnings():
         warnings.filterwarnings(
-            'ignore',
+            "ignore",
             category=UserWarning,
-            message='Some epochs have no duration',
+            message="Some epochs have no duration",
         )
         warnings.filterwarnings(
-            'ignore',
+            "ignore",
             category=RuntimeWarning,
-            message='divide by zero encountered in scalar divide',
+            message="divide by zero encountered in scalar divide",
         )
     return clusters[
-        (clusters['isi_violations_ratio'] < 0.5)
-        & (clusters['presence_ratio'] > 0.9)
-        & (clusters['firing_rate'] > 0.5)
-        & (clusters['snr'] > 1)
+        (clusters["isi_violations_ratio"] < 0.5)
+        # & (clusters['amplitude_cutoff'] < 0.1)
+        & (clusters["presence_ratio"] > 0.9)
+        & (clusters["firing_rate"] > 0.5)
+        & (clusters["snr"] > 1)
     ]
+
