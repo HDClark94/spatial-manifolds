3ef913ab0289b4050e5e0043939e0e230c02b27b
HDClark94 <harrydclark91@gmail.com>
2025-07-03 10:45:34 +0100

    typo

--- scripts/eddie/test_script.sh
+++ scripts/eddie/test_script.sh
@@ -2,4 +2,4 @@
 #$ -cwd -pe sharedmem 1 -l h_vmem=19G,h_rt=0:29:59 -N my_job
 
 cd /exports/eddie/scratch/hclark3/spatial-manifolds/scripts/eddie
-/home/hclark3/.local/bin/uv run test_eddie_script.py
+/home/hclark3/.local/bin/uv run test_eddie_scipt.py
