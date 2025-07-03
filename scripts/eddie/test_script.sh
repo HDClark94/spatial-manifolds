dd62eb4445d5dfc7b9924b2c8036a7b2da9d33fa
HDClark94 <harrydclark91@gmail.com>
2025-07-03 10:17:29 +0100

    eddie pathing

--- scripts/eddie/test_script.sh
+++ scripts/eddie/test_script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #$ -cwd -pe sharedmem 1 -l h_vmem=19G,h_rt=0:29:59 -N my_job
 
-/home/hclark3/.local/bin/uv run /exports/eddie/scratch/hclark3/spatial-manifolds/scripts/eddie/test_eddie_script.py
+cd /exports/eddie/scratch/hclark3/spatial-manifolds/scripts/eddie/
+/home/hclark3/.local/bin/uv run test_eddie_script.py
