3aceca7d867cc081b6cde808150a6675077d4189
HDClark94 <harrydclark91@gmail.com>
2025-07-14 18:32:37 +0100

    NGS are not in VISUAL CORTEX

--- src/spatial_manifolds/detect_grids.py
+++ src/spatial_manifolds/detect_grids.py
@@ -233,7 +233,7 @@ def cell_classification_of1(mouse, day, percentile_threshold=99, source_path=Non
 
             if (max_grid_score_of1 > percentile99_grid_score_of1) and (spatial_info > percentile99_spatial_information_of1):
                 grid_cells = pd.concat([grid_cells, cell], ignore_index=True)
-            elif (spatial_info > percentile99_spatial_information_of1) and brain_region.str.contains('VIS'):
+            elif (spatial_info > percentile99_spatial_information_of1) and not brain_region.str.contains('VIS'):
                 non_grid_cells = pd.concat([non_grid_cells, cell], ignore_index=True)
             elif (speed_correlation > percentile99_speed_information_of1_pos) or (speed_correlation < percentile99_speed_information_of1_neg):
                 speed_cells = pd.concat([speed_cells, cell], ignore_index=True)
