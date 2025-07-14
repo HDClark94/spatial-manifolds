e02e539de61ed3b90832a21f133ec241de629aed
HDClark94 <harrydclark91@gmail.com>
2025-07-14 22:52:27 +0100

    set all sessions to run on eddie

--- scripts/eddie/run_eddie_GC_xgboost_distance_assay.py
+++ scripts/eddie/run_eddie_GC_xgboost_distance_assay.py
@@ -158,8 +158,8 @@ mouse_days = {20: [14,15,16,17,18,19,20,21,22,23,24,25,26],
               28: [16,17,18,19,20,21,22,23,24],
               29: [16,17,18,19,20,21,22,23,25],
             }
-
-mouse_days = {25: [24]} # remove once this is working 
+        
+#mouse_days = {25: [24]} # remove once this is working 
 #mouse_days = {29: [25]} # remove once this is working 
 
 for assay_mode in ["GC", "NGS"]:
