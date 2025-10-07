db515ac51d8c7abc7b278bfb60b041098a126453
HDClark94 <harrydclark91@gmail.com>
2025-10-07 10:28:25 +0200

    add this

--- scripts/figures/xgboost_task_anchoring_assay.py
+++ scripts/figures/xgboost_task_anchoring_assay.py
@@ -20,7 +20,7 @@ warnings.filterwarnings('ignore')
 
 time_start = time.time()
 
-use_parser = False
+use_parser = True 
 
 source_path = '/Users/harryclark/Downloads/COHORT12/'
 data_path = '/Users/harryclark/Documents/data/'
