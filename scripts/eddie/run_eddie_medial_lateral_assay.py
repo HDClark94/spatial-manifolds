fab156d6be98d4ac7b13bc5478d74ffdbe9da57b
HDClark94 <harrydclark91@gmail.com>
2025-11-25 13:56:06 +0000

    medial lateral assay

---
+++ scripts/eddie/run_eddie_medial_lateral_assay.py
@@ -0,0 +1,212 @@
+from datetime import datetime
+import subprocess
+import pandas as pd 
+import pynapple as nap
+import warnings
+import os
+
+def curate_clusters(clusters) -> nap.TsGroup:
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            category=UserWarning,
+            message="Some epochs have no duration",
+        )
+        warnings.filterwarnings(
+            "ignore",
+            category=RuntimeWarning,
+            message="divide by zero encountered in scalar divide",
+        )
+    return clusters[
+        (clusters["isi_violations_ratio"] < 0.5)
+        # & (clusters['amplitude_cutoff'] < 0.1)
+        & (clusters["presence_ratio"] > 0.9)
+        & (clusters["firing_rate"] > 0.5)
+        & (clusters["snr"] > 1)
+    ]
+
+
+def run_python_script(python_arg, username, venv=None, cores=None, email=None, h_rt=None, h_vmem=None, hold_jid=None, script_file_path=None, staging=False, job_name=None):
+
+    if job_name is None:
+        job_name = "run_python"
+    
+    script_content = make_run_python_script(python_arg, username, venv=venv, cores=cores, email=email, h_rt=h_rt, h_vmem=h_vmem, hold_jid=hold_jid, staging=staging, job_name=job_name)
+    
+    if script_file_path is None:
+        script_file_path = f"{job_name}" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".sh"
+
+    save_script(script_content, script_file_path)
+    run_script(script_file_path)
+
+    return
+
+
+def run_stage_script(stageout_dict, script_file_path=None, hold_jid=None, job_name=None):
+
+    if hold_jid is not None:
+        hold_script = f" -hold_jid {hold_jid}"
+    if job_name is None:
+        job_name = "stage"
+
+    name_script = f" -N {job_name}"
+
+    """
+    makes a stage out script from a stageout_dict of the form
+    {'path/to/file/on/eddie': 'path/to/destination/on/datastore'}
+    Note: let's never stageout to the raw data folder, to avoid risk of deletion
+    """
+
+    script_text=f"""#!/bin/sh
+#$ -cwd
+#$ -q staging
+#$ -l h_rt=00:29:59{hold_script}{name_script}"""
+
+    if script_file_path is None:
+        script_file_path = f"{job_name}" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".sh"
+
+
+    for source, dest in stageout_dict.items():
+        script_text = script_text + "\ncp -rn " + str(source) + " " + str(dest)
+    
+    save_script(script_text, script_file_path)
+    run_script(script_file_path)
+
+    return 
+
+def make_run_python_script(python_arg, username, venv=None, cores=None, email=None, h_rt=None, h_vmem=None, hold_jid=None, job_name=None, staging=False):
+    """
+    Makes a python script, which will run
+    >>  python python_arg
+
+    If nothing else is supplied, this will run on the venv 'elrond' with 19 cores, 19GB of RAM per core, with a 
+    hard runtime limit of 48 hours.    
+    """
+
+    if hold_jid is not None:
+        hold_script = f" -hold_jid {hold_jid}"
+    else:
+        hold_script = ""
+    if email is not None:
+        email_script = f" -M {email} -m e"
+    else:
+        email_script = ""
+    if venv is None:
+        venv = "elrond"
+    
+    if cores is None:
+        cores = 32
+
+    if h_rt is None:
+        h_rt = "47:59:59"
+    if h_vmem is None:
+        h_vmem=19
+    if job_name is not None:
+        name_script = f" -N {job_name}"
+    else:
+        name_script = ""
+    if staging:
+        staging_script = " -q staging"
+        core_script = ""
+        vmem_script = ""
+    else:
+        staging_script = ""
+        core_script = f" -pe sharedmem {cores}"
+        vmem_script = f",h_vmem={h_vmem}G"
+
+    script_content = f"""#!/bin/bash
+#$ -cwd{staging_script}{core_script} -l rl9=true{vmem_script},h_rt={h_rt}{hold_script}{email_script}{name_script}
+source $HOME/.bashrc
+/home/{username}/.local/bin/uv run {python_arg}"""
+
+    return script_content
+
+def save_script(script_content, script_file_path):
+
+    if script_file_path is None:
+        script_file_path = f"run_" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".sh"
+
+    f = open(script_file_path, "w")
+    f.write(script_content)
+    f.close()
+
+    return
+
+def run_script(script_file_path):
+
+    compute_string = "qsub " + script_file_path
+    subprocess.run( compute_string.split() )
+
+    return
+
+
+#=================================================================================================================
+
+#=================================================================================================================
+
+#=================================================================================================================
+
+#=================================================================================================================
+
+#=================================================================================================================
+
+
+#=================================================================================================================
+
+
+#=================================================================================================================
+
+
+#=================================================================================================================
+
+
+#=================================================================================================================
+
+
+
+#=================================================================================================================
+
+
+
+#=================================================================================================================
+
+
+
+
+#=================================================================================================================
+#=================================================================================================================
+mouse_days = {28: [25]
+            }
+
+
+# there are sessions with detected grid modules
+mouse_days = {21: [16,21,25,26],
+              20: [25],
+              25: [19, 20,21,22,24,25],
+              26: [15, 18,19],
+              27: [20,23,24,26],
+              28: [17,19,23],
+              29: [19,20,23,25],
+            }
+
+# multishank sessions only
+mouse_days = {21: [15,16,17,18,22,25,26],
+              20: [14,15,16,17,18,19],
+              22: [37,38,39,40,41,42,43,44],
+              25: [16,17,18,25,26],
+              26: [13,15,16,19,20,21,22],
+              27: [16,17,18,19,23,24,26],
+              28: [16,17,18,23],
+              29: [16,17,18,20,23,25],
+            }
+
+source_path = '/exports/eddie/scratch/hclark3/COHORT12/'
+for mouse, days in mouse_days.items():
+    for day in days:
+        data_path = f"/exports/eddie/scratch/hclark3/COHORT12/multishank_xgboost/"
+        job_name = f"M{mouse}D{day}_MSX"
+
+        run_python_script(f"/exports/eddie/scratch/hclark3/spatial-manifolds/scripts/figures/figure_3_xgboost/xgboost_medial_lateral_population_assay.py --mouse={mouse} --day={day} --data_path={data_path}", username="hclark3", email="hclark3@ed.ac.uk", cores=16, job_name=job_name)
+        #run_stage_script(stageout_dict, hold_jid=job_name)
+
+print('This script does not run any jobs, it is just a template for running jobs on Eddie.')
---
+++ scripts/figures/figure_3_xgboost/xgboost_medial_lateral_population_assay.py
@@ -0,0 +1,175 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+import pynapple as nap
+from spatial_manifolds.toroidal import *
+from spatial_manifolds.behaviour_plots import *
+from spatial_manifolds.mlencoding import *
+from spatial_manifolds.circular_decoder import circular_decoder, cross_validate_decoder, cross_validate_decoder_time, circular_nanmean
+from spatial_manifolds.data.curation import curate_clusters
+from scipy.stats import zscore
+from spatial_manifolds.util import gaussian_filter_nan
+from spatial_manifolds.predictive_grid import compute_travel_projected, wrap_list
+from spatial_manifolds.behaviour_plots import *
+from spatial_manifolds.detect_grids import *
+from argparse import ArgumentParser
+
+import warnings
+warnings.filterwarnings('ignore')
+
+'''
+This script performs an xgboost assay to assess the influence of grid cells or non grid cells 
+on the encoding of a reference cell in a VR environment with respect to the distance of the grid cells
+or non grid cells to the reference cell.
+It uses a subset of grid cells and non-grid spatial cells to predict the reference cells firing 
+based on their activity, position and the history of their activity and position. 
+The results are saved in a YAML file for further analysis.
+
+This assay is will be optimised for recordings which were recorded in the multishank mode ||||
+'''
+
+use_parser = True
+
+source_path = '/Users/harryclark/Downloads/COHORT12/'
+data_path = '/Users/harryclark/Documents/data/'
+fig_path = '/Users/harryclark/Documents/figs/FIGURE1/'
+mouse = 25
+day = 25
+
+if use_parser:
+    parser = ArgumentParser()
+    parser.add_argument('--mouse', type=int, required=True, help='Mouse ID')
+    parser.add_argument('--day', type=int, required=True, help='Day of recording')
+    parser.add_argument('--data_path', type=str, required=True, help='Path to data directory')
+    args = parser.parse_args()
+
+    mouse = args.mouse
+    day = args.day
+    data_path = args.data_path
+    source_path = '/exports/eddie/scratch/hclark3/COHORT12/'
+ 
+
+# xgboost parameters 
+nfilters = 5 # number of features to represent the covariate history per covariate
+history_length = 1000 # in ms
+
+locations = pd.read_csv(f'{source_path}/all_cluster_brain_locations_chris.csv')
+locations = locations[(locations['mouse'] == mouse) & (locations['day'] == day)]
+locations['coord_SCs_x'] = locations['coord_SCs_x'] *-1  # correct for the flipped SCs x coordinates
+gcs, ngs, ns, sc, ngs_ns, all = cell_classification_of1(mouse, day, percentile_threshold=95, source_path=source_path) # subset
+
+# load the behaviour data
+tcs, tcs_time, _, last_ephys_bin, beh, clusters = compute_vr_tcs(mouse,day, apply_zscore=False, apply_guassian_filter=False, source_path=source_path)
+last_ephys_time_bin = clusters[clusters.index[0]].count(bin_size=time_bs, time_units = 'ms').index[-1]
+
+# time binned variables for later
+ep = nap.IntervalSet(start=0, end=last_ephys_time_bin, time_units = 's')
+speed_in_time = np.array(beh['S'].bin_average(bin_size=time_bs, time_units = 'ms', ep=ep))
+dt_in_time = np.array(beh['travel'].bin_average(bin_size=time_bs, time_units = 'ms', ep=ep)-((beh['trial_number'][0]-1)*tl))
+pos_in_time = dt_in_time%tl
+trial_number_in_time = (dt_in_time//tl)+beh['trial_number'][0]
+
+if np.any(np.isnan(pos_in_time)):
+    series = pd.Series(dt_in_time)
+    filled_series = series.ffill().bfill()
+    dt_in_time = np.array(filled_series)
+    pos_in_time = dt_in_time%tl
+    trial_number_in_time = (dt_in_time//tl)+beh['trial_number'][0]
+
+if np.any(np.isnan(speed_in_time)):
+    series = pd.Series(speed_in_time)
+    filled_series = series.ffill().bfill()
+    speed_in_time = np.array(filled_series)
+
+# create the reference cell population cluster ids
+target_cluster_ids = gcs.cluster_id.values.astype(int)
+
+# set up xgboost history model
+xgb_history = MLencoding(tunemodel = 'xgboost', cov_history = True, spike_history=False, 
+                         window = time_bs, n_filters = nfilters, max_time = history_length)
+ 
+# loop over all grid cells in a session
+results_df = pd.DataFrame()
+for target_id in target_cluster_ids:
+    
+    # determione if the grid cell is medial, middle or lateral MEC (250 um bins) starting at 3000
+    target_location = locations[(locations['mouse'] == mouse) & (locations['day'] == day) & (locations['cluster_id'] == target_id)]
+    target_ML_position = target_location['coord_SCs_x'].iloc[0]
+    if (target_ML_position <= 3000 and target_ML_position < 3400):
+        target_pos_label = 'medial_MEC'
+    elif (target_ML_position >= 3400 and target_ML_position < 3800):
+        target_pos_label = 'lateral_MEC'
+    else:
+        continue  # skip cells outside medial and lateral MEC
+
+    print(f'Processing target cell {target_id} located at {target_ML_position} um in {target_pos_label}')
+    
+    # take the minimum number of cells in either medial or lateral MEC NGS to balance the assay
+    min_cells = min([
+        len(
+            ngs[ngs['cluster_id'].isin(
+                locations[
+                    (locations['brain_region'].str.contains('ENT')) &
+                    (locations['coord_SCs_x'] >= bounds[0]) &
+                    (locations['coord_SCs_x'] < bounds[1])
+                ]['cluster_id']
+            )]
+        )
+        for bounds in [(3000, 3400), (3400, 3800)]
+    ])
+
+    for covariate_pos_label, cov_bounds in zip(['medial_MEC', 'lateral_MEC'], 
+                                               [(3000, 3400), (3400, 3800)]):
+        # get NGS covariate cells based on their ML position
+        covariate_cluster_ids = ngs.cluster_id.values.astype(int)
+        covariate_cluster_ids = covariate_cluster_ids[np.isin(covariate_cluster_ids, locations[
+                                                                    (locations['brain_region'].str.contains('ENT')) & 
+                                                                    (locations['coord_SCs_x'] >= cov_bounds[0]) & 
+                                                                    (locations['coord_SCs_x'] < cov_bounds[1])]['cluster_id'].values.astype(int))]
+        
+        cov_clusters_df = all[all.cluster_id.isin(covariate_cluster_ids)]
+
+        if len(covariate_cluster_ids) > 0:
+            cov_tcs_time = {cluster_id: tcs_time[cluster_id] for cluster_id in cov_clusters_df.cluster_id if cluster_id in tcs_time}
+            all_x = np.vstack(list(cov_tcs_time.values())).T
+            
+            for include_position in [True, False]:
+                x = all_x.copy()
+                # randomly select covariate cells to match the minimum number of cells
+                # do this by shuffling the all_x array along the second axis and then selecting the first min_cells columns
+                np.random.seed(0)
+                np.random.shuffle(x.T)
+                x = x[:, :min_cells]
+
+                if include_position:
+                    x = np.column_stack((pos_in_time, x))
+
+                # get the target variable
+                y = np.array(tcs_time[target_id])
+
+                # fit the model
+                Y_hat, pR2_cv = xgb_history.fit_cv(x, y, verbose = 0, continuous_folds = True)
+
+                print(f'Target ID: {target_id}, Target Pos: {target_pos_label}, Covariate Pos: {covariate_pos_label}, Include Position: {include_position}, pR2_cv = {np.nanmean(pR2_cv)}, using {min_cells} covariate cells')                
+                
+                tmp = pd.DataFrame()
+                avg_pR2 = np.nanmean(pR2_cv)
+                tmp['target_cluster_id'] = [target_id]
+                tmp['target_cell_type'] = ['GC']
+                tmp['target_pos_label'] = [target_pos_label]
+                tmp['covariate_pos_label'] = [covariate_pos_label]
+                tmp['covariate_cell_type'] = ['NGS']
+                tmp['n_covariate_cells'] = [len(covariate_cluster_ids)]
+                tmp['include_position'] = [include_position]
+                tmp['pR2_cv'] = [avg_pR2]
+                tmp['mouse'] = [mouse]
+                tmp['day'] = [day]
+
+                results_df = pd.concat([results_df, tmp], ignore_index=True)
+            else:
+                print(f'No covariate cells found for position {covariate_pos_label}, skipping.')
+
+results_df.to_pickle(f'{data_path}/xgboost_MEC_lateralisation_assay{mouse}_day{day}.pkl')
+print(f'Results saved successfully @ {data_path}/xgboost_MEC_lateralisation_assay{mouse}_day{day}.pkl')
+
