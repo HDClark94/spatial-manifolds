0083b4c4ef4e5bb33e7ff5c4728abed7e0a68717
HDClark94 <harrydclark91@gmail.com>
2026-04-17 14:19:36 +0700

    eddie

--- scripts/eddie/run_eddie_cell_number_assay_extra.py
+++ scripts/eddie/run_eddie_cell_number_assay_extra.py
@@ -192,6 +192,6 @@ for assay_mode in ["GC", "NGS"]:  # 'GC' for grid cells, 'NGS' for non grid spat
             stageout_dict = {
                 data_path: f'/exports/cmvm/datastore/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/Harry/SpatialLocationManifolds2025/data/xgboost_cell_number_assay_{assay_mode}/'
             }
-            job_name = f"M{mouse}D{day}_xgboost_{assay_mode}"
+            job_name = f"M{mouse}D{day}X_{assay_mode}"
             run_python_script(f"/exports/eddie/scratch/hclark3/spatial-manifolds/scripts/figures/xgboost_cell_number_assay_extra.py --mouse={mouse} --day={day} --assay_mode={assay_mode} --data_path={data_path}", username="hclark3", email="hclark3@ed.ac.uk", cores=16, job_name=job_name)
             run_stage_script(stageout_dict, hold_jid=job_name)
